28348d1f5771c9096dd635338627f1d12abe57cb
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-08 22:39:30 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@views/HomeView.vue'),
-    meta: {
-      title: 'Home'
-    }
-  }
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior() {
-    return { top: 0 };
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/accommodation',
+      name: 'accommodation',
+      component: () => import('../views/Accommodation.vue')
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import('../views/Services.vue')
+    },
+    {
+      path: '/tours',
+      name: 'tours',
+      component: () => import('../views/Tours.vue')
+    },
+    {
+      path: '/rentals',
+      name: 'rentals',
+      component: () => import('../views/Rentals.vue')
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: () => import('../views/Contacts.vue')
+    },
+    // Catch all for 404
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('../views/NotFound.vue')
+    }
+  ],
+  // Прокрутка наверх при переходе
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
   }
-});
+})
 
-router.beforeEach((to, from, next) => {
-  if (to.meta?.title) {
-    document.title = to.meta.title;
-  }
-  next();
-});
-
-export default router;
+export default router
