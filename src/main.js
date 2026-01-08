399054cc2d70c1ae17fb87d32482c5fc521f4ea7
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-08 23:08:32 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,16 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// --- ИСПРАВЛЕНИЕ 1: Подключение CSS файлов ---
-// Перемести папку 'css' в 'src/assets/css' перед запуском!
-import './assets/css/variables.css'
-import './assets/css/layout.css'
-import './assets/css/components.css'
-import './assets/css/buttons.css'
-import './assets/css/pages.css'
-import './assets/css/style.css'
-
-// Подключение основных стилей (Tailwind)
+// Подключаем только одну точку входа для стилей
 import './assets/styles/main.css'
 
 const app = createApp(App)
