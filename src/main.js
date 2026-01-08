da2c67fab6b1dac1774f8b0e486c5e4c9664f0d3
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-09 01:43:38 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
-// Подключаем только одну точку входа для стилей
-import './assets/styles/main.css'
+import './assets/styles/main.css' 
 
 const app = createApp(App)
 
