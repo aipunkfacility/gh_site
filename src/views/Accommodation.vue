ee566d8fb85b62a3871e82f914a8803da0bd1925
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-08 19:17:40 +0700

    Create Accommodation.vue

---
+++ src/views/Accommodation.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="section-title">Проживание</h1>
+      <AccommodationCard />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import AccommodationCard from '@components/accommodation/AccommodationCard.vue';
+</script>
