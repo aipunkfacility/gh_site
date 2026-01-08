140971fd802fe48cd1852da74f09602e6aef536c
aipunkfacility <ai.punk.facility@gmail.com>
2026-01-09 02:41:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // Основной алиас на src
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      // Дополнительные алиасы для совместимости с твоим кодом
-      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
